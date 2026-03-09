12fb6f23f155736b6aa38fc7eaa23fafcb998d7f
Gabrijel <gculina23@unizd.hr>
2026-03-09 18:17:29 +0100

    S2 solved

--- Fundamentals/src/pckg_cls_obj/AppTest.java
+++ Fundamentals/src/pckg_cls_obj/AppTest.java
@@ -6,5 +6,6 @@ public class AppTest {
         Student s1=new Student("Petar", "Petrić", 12);
         Student s2=new Student("Franka", "Franić", 43);
         s1.subscribeToStudentService();
+        s2.subscribeToStudentService();
     }
 }
